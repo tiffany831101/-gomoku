a10e3432e69b32a7232cd723c3f62abdd262cac2
tiffany <tiffany831101@gmail.com>
2019-01-01 13:08:18 +0800

    Updates

---
+++ precache-manifest.1334d9d6d21800001d9b4bf1c123afbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0aa9fe9f146cc1558b9",
+    "url": "/gomoku/static/js/runtime~main.b0aa9fe9.js"
+  },
+  {
+    "revision": "b7d636fdc07ab973fe0e",
+    "url": "/gomoku/static/js/main.b7d636fd.chunk.js"
+  },
+  {
+    "revision": "ac6e60a8489be1eb0bfb",
+    "url": "/gomoku/static/js/1.ac6e60a8.chunk.js"
+  },
+  {
+    "revision": "b7d636fdc07ab973fe0e",
+    "url": "/gomoku/static/css/main.ea5499d6.chunk.css"
+  },
+  {
+    "revision": "82436667d73e01bd1dc1f5903dfc4aba",
+    "url": "/gomoku/index.html"
+  }
+];
